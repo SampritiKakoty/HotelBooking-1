84eed61a7975c6bc2bb3c5e880e0f793721644d0
Training_b6b.01.16 <Training_b6b.01.16@accenture.com>
2017-12-01 16:51:29 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule,HttpModule
+    BrowserModule,HttpModule,
+    RouterModule.forRoot([
+      {path:'home' , component: HomeComponent},
+      {path: 'search', component: SearchComponent},
+      {path: '**', component: HomeComponent}
+    ])
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
